d383e7f0afdcb932be64a5cbce6e0786ae388e00
YougaKing <yougakingwu@gmail.com>
2018-11-22 09:51:54 +0800

    数据库

--- bugly.js
+++ bugly.js
@@ -1,5 +1,6 @@
-const token = '1821107426';
+const token = '1341598638';
 const appId = 'd98a6960d7';
+const rows = 10;
 const host = 'bugly.qq.com';
 const baseUrl = 'https://' + host;
 
@@ -8,12 +9,12 @@ const headers = {
     'Connection': 'keep-alive',
     'X-token': token,
     'Accept': 'application/json;charset=utf-8',
-    'x-csrf-token': 'r8i_HF8OuXNOq5LId51SlQZK',
+    'x-csrf-token': 'cSlSsom8E5Q1ZkBmsDRc8wN2',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
     'Content-Type': 'application/json;charset=utf-8',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Cookie': 'pgv_pvi=1512451072; _ga=GA1.2.783254942.1539094158; btcu_id=d20fefd45e8e861c562af366cbf891b55bbcb68ff0018; ptui_loginuin=451477973; pt2gguin=o0451477973; RK=TSTgnUpUE0; ptcz=acf2c37e7aba43ff7bd996dcef1f38bb73c7cbf44e990667afcfd1258e4bd09d; vc=vc-a72c2ea9-7854-47ca-9e5d-e89b34b58630; vc.sig=B98fDS1eLwALUlSD94_exbuVtQ9zPB6HD-__Okyp3ss; tvfe_boss_uuid=1c91433946473eb6; pgv_pvid=65861837; o_cookie=451477973; token-skey=47915048-1f85-8b36-69c9-0af11072d524; token-lifeTime=1542826333; _gid=GA1.2.843645078.1542808331; _gat=1; pgv_si=s8318619648; _qpsvr_localtk=0.6773126282688078; ptisp=ctc; uin=o0451477973; skey=@RwkTDA7cj; NODINX_SESS=hzZOKZnGEpkB5rZUubHL6bL7ndan47zvBeD73780FRfRHmrMvzxrcWw0rF5-HP7l; csrfToken=r8i_HF8OuXNOq5LId51SlQZK; referrer=eyJpdiI6InRsaE1JMHdlb2dOblh3YTdYMGQyUVE9PSIsInZhbHVlIjoid1JUR3ZtcHZyclFvWG9odlp0XC93WXBZT3h2bjRcL0JJcG5LR1ZMaXpIQ0NzQWtHaVhPQVdaa1FIUGVuMlVEZGxLZ3ZLMzNodThNRG9GMDhRYytSemJFMzJsSUFQVzNnNEZiQnVDRnk2T3FpbzUzUmVqeU5oamUxMHdLRFR4bU9rdk1naktSU1RcL0JnUkRJMHlXcXFEeXY4d2ExRlhEeDlJVE5FeUlpZ2VlTjBzPSIsIm1hYyI6IjcyZDJhNTRiZTFlZDA0ZmQ0OTYwMjU4YzRkNGI2NGU5MmZmYTgxODFlYTNkMDE0ZmI0MzY5ZDg2MmIyM2EwYWMifQ%3D%3D; bugly_session=eyJpdiI6ImtTc0VsYU9lSHdzakl2Y3Bzcnl1eHc9PSIsInZhbHVlIjoiUXVHNkNacjcxeVNsZFYrT0dGdGwzS0lVNlJiaE8rWjZZQ2wyQWNpdlJzNWFtd1hycFhyNlwvSHFjekRUT0cyNkZaMDBzd1hNRmJYcXA3dnZxSXdxRTZBPT0iLCJtYWMiOiI1ZjdiZWVmZTQ3YjQ4N2UxZGQxNmM4ZjI4Yjk1YzMyYzBjODYyZjI3MTQwNDE4MDg2ODdlZTY3YTFlY2RkNTM4In0%3D'
+    'Cookie': 'btcu_id=ed70f24fdc9235e8bc045c4c55a836755aea9eb6b0dc6; pgv_pvi=1553976320; RK=kbSoz2plS0; ptcz=79a4069068d1673ba15d973f7e7aca9455f95abbde9b44625167c8361d34bbc6; pgv_pvid=1021383505; tvfe_boss_uuid=a9fb74b55ead28c8; o_cookie=451477973; _ga=GA1.2.877053183.1526950312; vc=vc-e5e3e667-5be2-4463-8c1d-2bc4dc3133d7; btcu_id=fbe3a52d-887e-4f0b-87eb-8248ec69fbe2; pac_uid=1_451477973; ptui_loginuin=451477973; pt2gguin=o0451477973; luin=o0451477973; lskey=000100006ccfc114d1c01a0a84922fe0c2be44e9a1fe3ac4e267cd91169aaa4da2eec75cb58f0c898619634d; _gid=GA1.2.203917385.1542588771; token-skey=ff91172e-9516-c8b9-ed64-6267cc72ebd3; token-lifeTime=1542866662; pgv_si=s5506009088; _qpsvr_localtk=0.9256885250429903; ptisp=cm; uin=o0451477973; skey=@dfhnzte26; csrfToken=cSlSsom8E5Q1ZkBmsDRc8wN2; NODINX_SESS=Zs2abcmpqd75YmySeKf9kH240H-tb7patm7Y56oR1Ui7bE4HwvrW3ZLTRXD_Neba; vc=vc-0f721cfe-4e64-4253-9892-44a1567c68a7; vc.sig=10lxrQM8fHrMNxFvvJSniOyuCiQO3AArTz6NNOOv52Y; _gat=1; referrer=eyJpdiI6InlYeDE4cGg1SXVoSWJRTXp5anpJcWc9PSIsInZhbHVlIjoiRzU5dHpuNDFNZjE0VlVFaUZzVVNOTUp0bGVvMGpLWlowd0xhZSs0MmhGQmRJVWdFaXBuOW5rSVNLZThLdzlQWVNibitjZ1haMjlVTXRjejE2K1VHMEZPQURhbFwvQU5ZV0JTK0hiakFGeHhqeXVQQW4rSEhJYUg3RzFOQkhqQlZzT2hqN1RzTXBiQjkyYUV1UzBNZVU4VmNURXVnUkpuTXJST3JyNGNVVHlSWT0iLCJtYWMiOiIwMzkxOGNiYjdhYjMzZDYwZmNmMGViNjM4YzRjNjY5ZDI2ZDZiNjdhMWRlNjQxZDdjNjVkY2IzMTVhODgwOTZlIn0%3D; bugly_session=eyJpdiI6IllWZk1KNzhzMGI5QmprK2QyWFZEOGc9PSIsInZhbHVlIjoiaGd3NnlBTmExbzBTYjVrWnZIM1R4STFPSDNxcWZMNU55SEdIYm5LVE42ZnJDWTB5RHd3aUVKRGdHS29PMVVkSTRaQWVBTk9NTGs4M1RVeGZcL1kwVWVRPT0iLCJtYWMiOiI3NjI5NTY5YTA0YTBlZjU1Y2VmYzQ4MDI1YzcwYmJjYjRkYmY3NGQ2NzA4NDE4NTc3YThiZmQyNGYxMTYyNDg0In0%3D'
 };
 
 const db = require('./db.js');
@@ -38,7 +39,7 @@ Bugly.getIssueList = function (start, sortOrder, sortField, version,) {
         '&pid=1' +
         '&platformId=1' +
         '&sortOrder=' + sortOrder +
-        '&rows=10' +
+        '&rows=' + rows +
         '&sortField=' + sortField +
         '&appId=' + appId +
         '&fsn=3c13fcc5-99ae-4050-99f2-b352b93eec34' +
@@ -69,9 +70,16 @@ Bugly.getIssueList = function (start, sortOrder, sortField, version,) {
                     Bugly.getCrashList(0, issue, version)
                 });
 
-                db.insertIssueList(issueList)
+                db.insertIssueList(issueList);
+
+                if (issueList.length === rows) {
+                    let issue = issueList[issueList.length - 1];
+                    Bugly.getIssueListForUploadTime(issue.index, version);
+                } else {
+                    console.log("issueList.length:" + issueList.length)
+                }
             } else {
-                console.error(obj)
+                console.log(obj)
             }
         });
 };
@@ -86,7 +94,7 @@ Bugly.getCrashList = function (start, issue, version) {
         '&pid=1' +
         '&platformId=1' +
         '&issueId=' + issue.issueId +
-        '&rows=10' +
+        '&rows=' + rows +
         '&appId=' + appId +
         '&fsn=3c13fcc5-99ae-4050-99f2-b352b93eec34' +
         (version ? ('&version=' + version) : '')
@@ -105,12 +113,21 @@ Bugly.getCrashList = function (start, issue, version) {
             }
             const obj = JSON.parse(response.text);
             if (isCrashListAvailable(obj)) {
+                let count = 0;
+                let index = start;
                 let crashDatas = obj.ret.crashDatas;
                 for (let key in crashDatas) {
+                    count++;
+                    index++;
                     Bugly.getCrashDoc(crashDatas[key])
                 }
+                if (count === rows) {
+                    Bugly.getCrashList(index, issue, version);
+                } else {
+                    console.log("crashList.length:" + count)
+                }
             } else {
-                console.error(obj)
+                console.log(obj)
             }
         });
 };
@@ -138,7 +155,7 @@ Bugly.getCrashDoc = function (crashData) {
                 crashMap._id = crashMap.crashId;
                 db.insertCrashMap(crashMap);
             } else {
-                console.error(obj)
+                console.log(obj)
             }
         });
 };
--- routes/crash.js
+++ routes/crash.js
@@ -5,18 +5,21 @@ const db = require('../db.js');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+    getIssueListForUploadTime();
+});
+
+
+function getIssueListForUploadTime() {
     let where = {"version": '7.4.0'};
     let sort = {"index": -1};
     db.queryIssueList(where, sort, function (result) {
         if (result.length > 0) {
             let issue = result[0];
             bugly.getIssueListForUploadTime(issue.index, '7.4.0');
-            res.json(result);
         } else {
             bugly.getIssueListForUploadTime(0, '7.4.0');
-            res.send('加载中...');
         }
     });
-});
+}
 
 module.exports = router;
